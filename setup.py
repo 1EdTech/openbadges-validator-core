264404193e0af35f0c41bd447bc79416483bdb8d
Nate Otto <notto@concentricsky.com>
2015-07-13 15:22:33 -0700

    v0.2.1-test8

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='badgecheck',
-    version='0.2.1-test7',
+    version='0.2.1-test8',
     packages=['badgecheck'],
     include_package_data=True,
     license='aGPL License',
