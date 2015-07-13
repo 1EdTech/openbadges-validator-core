7b70e8a5ee2ea00b92af2d67c85689c201b52c74
Nate Otto <notto@concentricsky.com>
2015-07-12 19:40:46 -0700

    update self's version in setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='badgecheck',
-    version='0.1.2',
+    version='0.2.1-test3',
     packages=['badgecheck'],
     include_package_data=True,
     license='aGPL License',
