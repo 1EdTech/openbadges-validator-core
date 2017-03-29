0c55e7e27c287680ca0bda5d25558edb04e459a1
Viktor Haag <Viktor.Haag@D2L.com>
2017-03-29 17:14:15 -0400

    Add caching requests-based document loader and tests
    - To avoid monkey-patching requests, use requests_cache.CachedSession
    
    - Create a callable class to maintain the caching preference in instance-state
      and use either CachedSession or requests.Session to do the URL-getting
    
      To use:
    
        from badgecheck.util import CachableDocumentLoader
    
        load_document_with_cache = CachableDocumentLoader(cachable=True)
        jsonld.compact(doc, context, options={'documentLoader': load_document_with_cache}
    
    - For now, stick with an in-memory cache that keeps responses for 300 seconds,
      and aggressively expunges expired items after each use of the loader
    
    - For auditing/testing it adds 'from_cache' field to the loaded doc dictionary
      when the cache is being used; pyld doesn't mind this added field to its
      loaded doc struct.
    
    - Build tests for the loader:
    
      * Test that it actually does cache
    
      * Test that without caching the loader still works
    
      * Test that pyld will accept the caching loader and successfully use the
        cache when trying to load the context URL from one of our test fixtures
    
    - Possible improvements: allow the constructor to accept any back-end known to
      requests_cache, or a back-end instance, to allow for persistent DB-based
      caches, etc.

---
+++ badgecheck/util.py
@@ -0,0 +1,48 @@
+import string
+from urlparse import urlparse
+
+from pyld.jsonld import JsonLdError
+import requests
+import requests_cache
+
+
+class CachableDocumentLoader(object):
+    def __init__(self, cachable=False):
+        self.cachable = cachable
+        if self.cachable:
+            self.session = requests_cache.CachedSession(backend='memory', expire_after=300)
+        else:
+            self.session = requests.Session()
+
+    def __call__(self, url):
+        try:
+            # validate URLs
+            pieces = urlparse(url)
+            if (not all([pieces.scheme, pieces.netloc]) or
+                    pieces.scheme not in ['http', 'https'] or
+                    set(pieces.netloc) > set(string.ascii_letters + string.digits + '-.:')):
+                raise JsonLdError(
+                    'Could not dereference URL; can only load URLs using',
+                    'the "http" and "https" schemes.',
+                    'jsonld.InvalidUrl', {'url': url},
+                    code='loading document failed')
+
+            response = self.session.get(
+                url, headers={'Accept': 'application/ld+json, application/json'})
+
+            doc = {'contextUrl': None, 'documentUrl': url, 'document': response.text}
+
+            if self.cachable:
+                doc['from_cache'] = response.from_cache
+                self.session.remove_expired_responses()
+
+            return doc
+
+        except JsonLdError as e:
+            raise e
+        except Exception as cause:
+            raise JsonLdError(
+                'Could not retrieve JSON-LD document from URL.',
+                'jsonld.LoadDocumentError',
+                code='loading document failed',
+                cause=cause)
