44dc259842a66fa493d35cb10e7d5a29736d8618
Viktor Haag <Viktor.Haag@D2L.com>
2017-04-28 13:58:24 -0400

    Verifier should find the canonical source behind JSON-string input
    - add tasks.input.find_id_in_jsonld utility function to accept a JSON string,
      compact it using the OpenBadges context, and then seek for an 'id' property:
      return either the found ID field or empty string
    
    - modify detect_input_type to use find_id_in_jsonld: when provided with a JSON
      string as input, seek for the ID in it; if found, store that URL as the
      assertion value in the state (replace the JSON string) and detect the type as
      "URI" and proceed as in that case; if no ID found, just set the input type to
      JSON and move on
    
    - update input_reducer to, upon STORE_INPUT, place any pre-existing value with
      the new provided value; always copy passed in state, and return a new state
      object.
    
    - tests around detect input type of JSON string: test the case where an ID can
      be found that's a URL from an assertion (will replace with URL); test the
      case where an ID can be found but it's not a URL (do not replace, as it won't
      be de-referencable)

--- badgecheck/tasks/input.py
+++ badgecheck/tasks/input.py
@@ -1,9 +1,12 @@
 import json
+from pyld import jsonld
 import re
 import validators
 
-from ..actions.input import set_input_type
+from ..actions.input import set_input_type, store_input
 from ..actions.tasks import add_task
+from ..openbadges_context import OPENBADGES_CONTEXT_V2_URI
+from ..utils import CachableDocumentLoader
 from task_types import FETCH_HTTP_NODE
 from utils import task_result
 
@@ -28,6 +31,14 @@ def input_is_jws(user_input):
     return bool(jws_regex.match(user_input))
 
 
+def find_id_in_jsonld(json_string):
+    input_data = json.loads(json_string)
+    options = {'documentLoader': CachableDocumentLoader(cachable=True)}
+    result = jsonld.compact(input_data, OPENBADGES_CONTEXT_V2_URI, options=options)
+    node_id = result.get('id','')
+    return node_id
+
+
 """
 Input-processing tasks
 """
@@ -44,8 +55,15 @@ def detect_input_type(state, task_meta=None):
         new_actions.append(set_input_type(detected_type))
         new_actions.append(add_task(FETCH_HTTP_NODE, url=input_value))
     elif input_is_json(input_value):
-        detected_type = 'json'
+        id_url = find_id_in_jsonld(input_value)
+        if input_is_url(id_url):
+            detected_type = 'url'
+            new_actions.append(store_input(id_url))
+        else:
+            detected_type = 'json'
         new_actions.append(set_input_type(detected_type))
+        if detected_type == 'url':
+            new_actions.append(add_task(FETCH_HTTP_NODE, url=id_url))
     elif input_is_jws(input_value):
         detected_type = 'jws'
         new_actions.append(set_input_type(detected_type))
--- tests/test_input.py
+++ tests/test_input.py
@@ -1,11 +1,14 @@
 import unittest
 
+import json
 from pydux import create_store
 
 from badgecheck.actions.input import set_input_type, store_input
 from badgecheck.reducers import main_reducer
 from badgecheck.state import INITIAL_STATE
 from badgecheck.tasks.input import detect_input_type
+
+from testfiles.test_components import test_components
 
 
 class InputReducerTests(unittest.TestCase):
@@ -37,4 +40,43 @@ class InputTaskTests(unittest.TestCase):
         self.assertEqual(len(actions), 2)
         self.assertEqual(actions[0]['type'], 'SET_INPUT_TYPE')
         self.assertEqual(actions[1]['url'], url)
+
+    def test_input_jsonld_type_detection_replaces_with_url(self):
+        """
+        The detect_input_type task should successfully detect JSONLD with an id URL
+        as input and switch to using an 'id' as URL value if possible
+        """
+        json_input = test_components['2_0_basic_assertion']
+        state = INITIAL_STATE.copy()
+        state['input']['value'] = json_input
+
+        success, message, actions = detect_input_type(state)
+
+        self.assertTrue(success)
+        self.assertEqual(len(actions), 3)
+        self.assertEqual(actions[0]['type'], 'STORE_INPUT')
+        self.assertEqual(actions[1]['type'], 'SET_INPUT_TYPE')
+        self.assertEqual(actions[2]['type'], 'ADD_TASK')
+        self.assertEqual(actions[2]['name'], 'FETCH_HTTP_NODE')
+
+        self.assertEqual(actions[0]['input'], actions[2]['url'])
+        self.assertEqual(json.loads(json_input)['id'], actions[2]['url'])
+
+    def test_input_jsonld_type_detection_preserves_json(self):
+        """
+        If the detect_input_type_task can't find an id field as a URL, it preserves
+        the input as json
+        """
+        assertion_dict = json.loads(test_components['2_0_basic_assertion'])
+        assertion_dict['id'] = assertion_dict['badge'] = u'urn:org:example:badges:robotics:beth'
+        json_input = json.dumps(assertion_dict)
+        state = INITIAL_STATE.copy()
+        state['input']['value'] = json_input
+
+        success, message, actions = detect_input_type(state)
+
+        self.assertTrue(success)
+        self.assertEqual(len(actions), 1)
+        self.assertEqual(actions[0]['type'], 'SET_INPUT_TYPE')
+        self.assertEqual(actions[0]['input_type'], 'json')
 
