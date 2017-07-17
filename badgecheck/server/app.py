530d1d75872df47a1dbd90b2b6cfd5ebac0fe4c8
Nate Otto <nate@ottonomy.net>
2017-07-17 16:43:51 -0700

    Establish basic JSON API capability.

--- badgecheck/server/app.py
+++ badgecheck/server/app.py
@@ -9,9 +9,19 @@ app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 4 * 1024 * 1024  # 4mb file upload limit
 
 
+def request_wants_json():
+    best = request.accept_mimetypes.best_match(['application/json', 'text/html'])
+    return best == 'application/json' and request.accept_mimetypes[best] > request.accept_mimetypes['text/html']
+
+
 @app.route("/")
 def home():
     return render_template('index.html')
+
+
+@app.route("/results", methods=['GET'])
+def resultGetRedirect():
+    return redirect('/')
 
 
 @app.route("/results", methods=['POST'])
@@ -21,6 +31,9 @@ def results():
         if 'image' in request.files and len(request.files['image'].filename):
             user_input = request.files['image']
         verification_results = verify(user_input)
+
+        if request_wants_json():
+            return (json.dumps(verification_results, indent=4), 200, {'Content-Type': 'application/json'},)
         return render_template(
             'results.html', results=json.dumps(verification_results, indent=4))
 
