9524455b0c173706ce20d05aa55290cd8897d9b5
Nate Otto <nate@ottonomy.net>
2017-09-30 16:16:14 -0700

    Validate endorsements as the input.

--- tests/test_validate_endorsements.py
+++ tests/test_validate_endorsements.py
@@ -47,6 +47,7 @@ class EndorsementTests(unittest.TestCase):
         self.endorsement = {
             '@context': OPENBADGES_CONTEXT_V2_URI,
             'id': 'http://example.org/endorsement',
+            'type': 'Endorsement',
             'claim': {
                 'id': self.badgeclass['id'],
                 'endorsementComment': 'Pretty good'
@@ -67,7 +68,7 @@ class EndorsementTests(unittest.TestCase):
         }
 
     @responses.activate
-    def test_discover_and_validate_claim_prop(self):
+    def test_validate_linked_endorsement(self):
         set_up_context_mock()
         self.set_up_resources()
 
@@ -78,6 +79,19 @@ class EndorsementTests(unittest.TestCase):
         results = verify(self.assertion['id'])
         self.assertTrue(results['report']['valid'])
         self.assertEqual(len(results['graph']), 5, "The graph now contains all five resources.")
+
+    @responses.activate
+    def test_validate_endorsement_as_input(self):
+        set_up_context_mock()
+        self.set_up_resources()
+
+        for resource in [self.assertion, self.badgeclass, self.issuer, self.endorsement, self.endorsement_issuer]:
+            responses.add(responses.GET, resource['id'], json=resource)
+        set_up_image_mock(self.badgeclass['image'])
+
+        results = verify(self.endorsement['id'])
+        self.assertTrue(results['report']['valid'])
+        self.assertTrue(len(results['graph']), 2)
 
     def test_claim_property_validation(self):
         self.set_up_resources()
