4ca0fffd12d49593e7d437f6c45f71c14973a371
Nate Otto <nate@ottonomy.net>
2018-09-22 16:15:13 -0700

    Improvement to test for #196 so it actually tests something.

--- tests/test_validate_endorsements.py
+++ tests/test_validate_endorsements.py
@@ -41,7 +41,7 @@ class EndorsementTests(unittest.TestCase):
             'id': 'http://example.com/badgeclass',
             'type': 'BadgeClass',
             'issuer': 'http://example.com/issuer',
-            'endorsement': ['http://example.org/endorsement','http://example.org/endorsement'],
+            'endorsement': ['http://example.org/endorsement', 'http://example.org/endorsement2'],
             'name': 'Best Badge',
             'description': 'An achievement that is good.',
             'image': 'http://example.com/badgeimage',
@@ -69,6 +69,8 @@ class EndorsementTests(unittest.TestCase):
                 'type': "HostedBadge"
             }
         }
+        self.endorsement2 = self.endorsement.copy()
+        self.endorsement2['id'] = 'http://example.org/endorsement2'
         self.endorsement_issuer = {
             '@context': OPENBADGES_CONTEXT_V2_URI,
             'id': 'http://example.org/issuer',
@@ -83,13 +85,26 @@ class EndorsementTests(unittest.TestCase):
         set_up_context_mock()
         self.set_up_resources()
 
-        for resource in [self.assertion, self.badgeclass, self.badgeclass_with_endorsement_array, self.issuer, self.endorsement, self.endorsement_issuer]:
+        for resource in [self.assertion, self.badgeclass, self.issuer, self.endorsement, self.endorsement_issuer]:
             responses.add(responses.GET, resource['id'], json=resource)
         set_up_image_mock(self.badgeclass['image'])
 
         results = verify(self.assertion['id'])
         self.assertTrue(results['report']['valid'])
         self.assertEqual(len(results['graph']), 5, "The graph now contains all five resources.")
+
+    @responses.activate
+    def test_validate_linked_endorsement_array(self):
+        set_up_context_mock()
+        self.set_up_resources()
+
+        for resource in [self.assertion, self.badgeclass_with_endorsement_array, self.issuer, self.endorsement, self.endorsement2, self.endorsement_issuer]:
+            responses.add(responses.GET, resource['id'], json=resource)
+        set_up_image_mock(self.badgeclass['image'])
+
+        results = verify(self.assertion['id'])
+        self.assertTrue(results['report']['valid'])
+        self.assertEqual(len(results['graph']), 6, "The graph now contains all six resources including both endorsements.")
 
     @responses.activate
     def test_validate_endorsement_as_input(self):
