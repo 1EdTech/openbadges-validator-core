1040a8d8c8592ea6226036d4491598442adbf3cd
Jeff Creed <jcreed@concentricsky.com>
2020-12-08 07:51:30 -0800

    make language test runnable, add depth

--- tests/validate_language.py
+++ tests/validate_language.py
@@ -1,25 +1,27 @@
 import unittest
 
 from openbadges.verifier.actions.tasks import add_task
-from openbadges.verifier.tasks import run_task
+from openbadges.verifier.tasks import task_named
 from openbadges.verifier.tasks.task_types import VALIDATE_EXPECTED_NODE_CLASS
 from openbadges.verifier.tasks.validation import OBClasses
 
 
 class ValidateLanguagePropertyTests(unittest.TestCase):
-    def validate_language_prop_basic(self):
+    def test_validate_language_prop_basic(self):
+        options = {'max_validation_depth': 3}
+
         badgeclass = {
             'id': 'http://example.org/badgeclass',
             '@language': 'en-US'
         }
         state = {'graph': [badgeclass]}
         task = add_task(VALIDATE_EXPECTED_NODE_CLASS, node_id=badgeclass['id'],
-                        expected_class=OBClasses.BadgeClass)
-        result, message, actions = run_task(state, task)
+                        expected_class=OBClasses.BadgeClass, depth=0)
+        result, message, actions = task_named(VALIDATE_EXPECTED_NODE_CLASS)(state, task, **options)
         self.assertTrue(result)
 
         l_actions = [a for a in actions if a.get('prop_name') == '@language']
         self.assertEqual(len(l_actions), 1)
 
-        result, message, actions = run_task(state, l_actions[0])
+        result, message, actions = task_named(l_actions[0]['name'])(state, l_actions[0], **options)
         self.assertTrue(result)
